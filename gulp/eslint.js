2f711c68ec0363c4a493ad4be669ef281f33654d
Ahmed Nuaman <ahmed@ahmednuaman.com>
2015-07-12 14:45:18 +0100

    fixed issue with missing module

--- gulp/eslint.js
+++ gulp/eslint.js
@@ -1,7 +1,9 @@
+var eslint = require('gulp-eslint');
+
 module.exports = function (config, gulp) {
   gulp.task('eslint', function () {
     return gulp.src(config.path.es6)
-               .pipe(require('gulp-eslint')({
+               .pipe(eslint({
                   configFile: require('path').join(config.base, 'eslint.config.json')
                 }))
                .pipe(eslint.format());
